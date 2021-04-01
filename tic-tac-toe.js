0cbb8755bec839b3829b7fa429bf7fe2fe46b928
oadeliyi1 <oriyomiadeliyi@gmail.com>
2021-04-01 19:17:59 -0400

    Game Status complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,72 @@
 let currentPlayerSymbol = 'x'
 const squareValues = ['', '', '', '', '', '', '', '', ''];
 
+let gameStatus = '';
+
+
 window.addEventListener('DOMContentLoaded', (event) => {
+
+  const checkGameStatus = () => {
+    if (squareValues[0] === squareValues[1] && squareValues[1] === squareValues[2] && squareValues[2] !== '') {
+      //top row
+      gameStatus = currentPlayerSymbol.toUpperCase();
+    }
+    if (squareValues[3] === squareValues[4] && squareValues[4] === squareValues[5] && squareValues[5] !== '') {
+      //middle row
+      gameStatus = currentPlayerSymbol.toUpperCase();
+    }
+    if (squareValues[6] === squareValues[7] && squareValues[7] === squareValues[8] && squareValues[8] !== '') {
+     //bottom
+      gameStatus = currentPlayerSymbol.toUpperCase();
+    }
+    if (squareValues[0] === squareValues[3] && squareValues[3] === squareValues[6] && squareValues[6] !== '') {
+         //left column
+      gameStatus = currentPlayerSymbol.toUpperCase();
+    }
+    if (squareValues[1] === squareValues[4] && squareValues[4] === squareValues[7] && squareValues[7] !== '') {
+        //middle column
+      gameStatus = currentPlayerSymbol.toUpperCase();
+    }
+    if (squareValues[2] === squareValues[5] && squareValues[5] === squareValues[8] && squareValues[8] !== '') {
+      //right column
+      gameStatus = currentPlayerSymbol.toUpperCase();
+    }
+    if (squareValues[0] === squareValues[4] && squareValues[4] === squareValues[8] && squareValues[8] !== '') {
+      //diagonal 1
+      gameStatus = currentPlayerSymbol.toUpperCase();
+    }
+    if (squareValues[2] === squareValues[4] && squareValues[4] === squareValues[6] && squareValues[6] !== '') {
+          //diagonal 2
+      gameStatus = currentPlayerSymbol.toUpperCase();
+    }
+
+    if (gameStatus === '') {
+      if (!(squareValues.includes(''))) {
+        gameStatus = 'None'
+      }
+    }
+
+    if (gameStatus !== '') {
+      document.getElementById('game-status').innerHTML = `Winner: ${gameStatus}`
+
+    }
+
+
+
+
+
+}
+
 
   let board = document.getElementById('tic-tac-toe-board');
 
+
+
   board.addEventListener('click', (event) => {
+
+    if (gameStatus !== '') {
+      return;
+    }
 
     let img = document.createElement('img');
     if (currentPlayerSymbol === 'x') {
@@ -26,6 +87,8 @@ window.addEventListener('DOMContentLoaded', (event) => {
     } else {
       event.preventDefault();
     }
+
+    checkGameStatus();
 
     if (currentPlayerSymbol === 'x') {
       currentPlayerSymbol = 'o';
