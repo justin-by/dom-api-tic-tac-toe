f64f251181cf2528ee809ba7eae74e4f3083d33b
oadeliyi1 <oriyomiadeliyi@gmail.com>
2021-04-01 17:45:42 -0400

    Player CLicks

---
+++ tic-tac-toe.js
@@ -0,0 +1,39 @@
+const currentPlayerSymbol = 'x'
+const squareValues = ['', '', '', '', '', '', '', '', ''];
+
+
+window.addEventListener('DOMContentLoaded', (event) => {
+
+  let board = document.getElementById('tic-tac-toe-board');
+
+  board.addEventListener('click', (event) => {
+
+
+    const targetID = event.target.id
+
+    if (targetID !== 'square-') {
+      event.preventDefault();
+
+
+    } else {
+      Number.parseInt(slice(7))
+
+
+    }
+
+
+
+
+  })
+
+let string - s
+
+
+
+
+
+
+
+
+
+ })
