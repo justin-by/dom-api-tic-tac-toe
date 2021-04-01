6e510623a5506f84c7e38f44148c3be91b2da7cd
justin <flaminlinkvp@gmail.com>
2021-04-01 15:38:57 -0700

    player clicks complete

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,5 @@
-const currentPlayerSymbol = 'x'
+let currentPlayerSymbol = 'x'
 const squareValues = ['', '', '', '', '', '', '', '', ''];
-
 
 window.addEventListener('DOMContentLoaded', (event) => {
 
@@ -8,17 +7,30 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
   board.addEventListener('click', (event) => {
 
-
-    const targetID = event.target.id
-
-    if (targetID !== 'square-') {
-      event.preventDefault();
-
+    let img = document.createElement('img');
+    if (currentPlayerSymbol === 'x') {
+      img.src = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-x.svg'
 
     } else {
-      Number.parseInt(slice(7))
+      img.src = 'https://assets.aaonline.io/Module-DOM-API/formative-project-tic-tac-toe/player-o.svg'
+    }
+    event.target.appendChild(img);
 
+    const targetID = event.target.id
+    if (targetID.includes('square-')) {
+      let number = Number.parseInt(targetID.slice(7))
+      if (squareValues[number] === '') {
+        squareValues[number] = currentPlayerSymbol;
+        console.log(squareValues);
+      }
+    } else {
+      event.preventDefault();
+    }
 
+    if (currentPlayerSymbol === 'x') {
+      currentPlayerSymbol = 'o';
+    } else {
+      currentPlayerSymbol = 'x';
     }
 
 
@@ -26,7 +38,6 @@ window.addEventListener('DOMContentLoaded', (event) => {
 
   })
 
-let string - s
 
 
 
@@ -34,6 +45,4 @@ let string - s
 
 
 
-
-
- })
+})
